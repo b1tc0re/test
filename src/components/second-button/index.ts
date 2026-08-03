0e675e65c59e5558f60acf80903c67189209e09e
Дмитрий <b1tc0re@yandex.ru>
2026-08-03 03:46:31 +0300

    feat(second-button): export supported size type

--- src/components/second-button/index.ts
+++ src/components/second-button/index.ts
@@ -1,2 +1,2 @@
 export { default as SecondButton } from './SecondButton.vue'
-export type { SecondButtonProps } from './second-button.types'
+export type { SecondButtonProps, SecondButtonSize } from './second-button.types'
