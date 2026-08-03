c2c553e1a60907caec5b2f8059d3b1adbdf422e4
Дмитрий <b1tc0re@yandex.ru>
2026-08-03 03:16:21 +0300

    fix(lint): allow Tailwind reference directive

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -2,4 +2,7 @@ export default {
   extends: ['stylelint-config-standard-scss'],
   ignoreFiles: ['build/**/*', 'node_modules/**/*'],
   overrides: [{ files: ['**/*.vue'], customSyntax: 'postcss-html' }],
+  rules: {
+    'scss/at-rule-no-unknown': [true, { ignoreAtRules: ['reference'] }],
+  },
 }
